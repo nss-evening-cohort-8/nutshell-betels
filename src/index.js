13d83cb036e1de21fcfa37fa05e634a17f6b7d6c
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-02 11:22:28 -0600

    axios call successful

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import loginButton from './components/Auth/auth';
 import checkLoginStatus from './helpers/authHelpers';
 
 import './index.scss';
+import zipCodeTest from './helpers/data/weatherData';
 
 const initialize = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   checkLoginStatus();
   loginButton();
+  zipCodeTest();
 };
 
 initialize();
