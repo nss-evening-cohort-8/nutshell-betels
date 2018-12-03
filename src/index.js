e2f45d0d83d1d32b4739968dfcd638825a010e4e
mohammad <khezri@outlook.com>
2018-12-03 01:16:30 -0600

    Created login page and nav bar

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import apiKeys from '../db/apiKeys.json';
 
 import loginButton from './components/Auth/auth';
 import checkLoginStatus from './helpers/authHelpers';
+import createNavbar from './components/Navbar/navbar';
 
 import './index.scss';
 
 const initialize = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  checkLoginStatus();
   loginButton();
+  checkLoginStatus();
+  createNavbar();
 };
 
 initialize();
